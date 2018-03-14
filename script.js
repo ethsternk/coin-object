560a550ce33670c00da33ab17b055011d0a9edf6
Ethan Sternke <ethan.sternke@gmail.com>
2018-03-14 09:47:20 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var coin = {
+    state: 0,
+    flip: function() {
+        this.state = Math.floor( Math.random()*2 );
+    },
+    toString: function() {
+        if (this.state === 0) { return "H" };
+        if (this.state === 1) { return "T" };
+    },
+    toHTML: function() {
+        var img = document.createElement("img");
+        if (this.state === 0) { img.src = "heads.jpg" };
+        if (this.state === 1) { img.src = "tails.jpg" };
+        return img;
+    }
+};
+
+for (let i = 0; i < 20; i++) {
+    coin.flip();
+    document.body.appendChild(coin.toHTML());
+}
